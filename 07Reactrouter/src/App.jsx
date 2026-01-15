74f5eb7f058f7862d525d69bfeb01d20cd694501
Danyal <dk3906445@gmail.com>
2026-01-15 07:37:26 -0800

    project-7

---
+++ 07Reactrouter/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <>
+    <h1 className='bg-green-600 text-black-600 font-bold text-center w-full p-4 rounded-lg'>React Router</h1>
+    </>
+  )
+}
